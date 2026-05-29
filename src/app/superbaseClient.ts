daf33c11290ee8afd7d1cb1f2d18813ccae8dd42
Michael Olajide <michaelolajidedavid@gmail.com>
2026-05-29 04:17:38 -0700

    fix: global replace search of old database endpoint domains

--- src/app/superbaseClient.ts
+++ src/app/superbaseClient.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "";
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "";
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://inygbyqptgrxngrmmpbv.supabase.co";
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImlueWdieXFwdGdyeG5ncm1tcGJ2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkwNjk4NjcsImV4cCI6MjA5NDY0NTg2N30.NqKtXPoXF-ayREJMylrC_fX531uSawD3e0tvNBCymUM";
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
